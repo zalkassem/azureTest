e66a19d1a6bd003d8fd9303874ce05fc393066fe
zalkassem <z.arab.sy@gmail.com>
2023-12-25 21:31:38 +0100

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -1,18 +1,3 @@
-# Configure mongodb.list file with the correct location
-#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-#echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-
-# Disable THP
-#sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
-#sudo echo never > /sys/kernel/mm/transparent_hugepage/defrag
-#sudo grep -q -F 'transparent_hugepage=never' /etc/default/grub || echo 'transparent_hugepage=never' >> /etc/default/grub
-
-# Install updates
-#sudo apt-get -y update
-
-#Install Mongo DB
-#sudo apt-get install -y mongodb-org
-
 sudo apt-get -y update
 sudo apt-get -y install nginx
 echo 'Hello World' | sudo tee /var/www/html/index.html
