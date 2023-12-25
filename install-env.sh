0c370247efdbdfb280ae46b154c2c8df05d4fbdf
zalkassem <z.arab.sy@gmail.com>
2023-12-25 23:19:54 +0100

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -2,3 +2,13 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 echo 'Hello World' | sudo tee /var/www/html/index.html
 sudo service nginx start
+
+sudo apt-get -y install git-all
+
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
+
