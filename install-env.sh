67857b8b6c6d6be53a93a9f704887d9587673fad
zalkassem <z.arab.sy@gmail.com>
2023-12-26 20:08:01 +0100

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -44,6 +44,7 @@ sudo nginx -t
 sudo systemctl restart nginx.service
 sudo systemctl status nginx.service
 sudo ufw enable
+sudo ufw allow 22/tcp
 sudo ufw allow 4004/tcp
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
