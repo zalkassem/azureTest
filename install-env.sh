4540321d67bc2d273a5d7076a5969916c09c0879
zalkassem <z.arab.sy@gmail.com>
2023-12-26 11:21:48 +0100

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -2,21 +2,16 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 echo 'Hello Zeiad from script World' | sudo tee /var/www/html/index.html
 sudo service nginx start
-
 sudo apt-get -y install git-all
-
 sudo apt-get install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install nodejs -y
-
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg  --dearmor
 cat > /etc/apt/sources.list.d/mongodb-org-7.0.list << EOF
 EOF
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo systemctl enable mongod
-sudo systemctl start mongod
