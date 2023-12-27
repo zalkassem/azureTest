88b9125f1a4d1cd8fa17efbdf743729ce110cd3d
zalkassem <z.arab.sy@gmail.com>
2023-12-27 11:58:58 +0100

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -15,8 +15,3 @@ EOF
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-cd /etc
-sudo sed -i 's/#security:/security:\n  authorization: enabled/g' mongod.conf
-sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' mongod.conf
-sudo systemctl enable mongod
-sudo systemctl start mongod
