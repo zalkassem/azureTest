dbe9a3ee206f529ed36db7a3f2a42939a885eb21
zalkassem <z.arab.sy@gmail.com>
2024-01-06 12:04:28 +0100

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -27,10 +27,10 @@ sudo systemctl start mongod
 #mongosh --eval "printjson(exit)"
 #################################################
 mongosh --quiet  --host 127.0.0.1 --port 27017
-mongosh  --eval "EJSON.stringify(db.getSiblingDB('admin'));"
-mongosh  --eval "EJSON.stringify(use admin));"
-mongosh  --eval 'EJSON.stringify(db.createUser({ user: "admin" , pwd: "$2",roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]})));'
-mongosh  --eval "EJSON.stringify(exit));"
+echo "db.getSiblingDB('admin'));"
+echo "use admin"
+echo 'db.createUser({ user: "admin" , pwd: "$2",roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"]})'
+echo "exit"
 cd /opt
 sudo git clone https://github.com/zalkassem/wex.git
 sudo mv wex wexcommerce
