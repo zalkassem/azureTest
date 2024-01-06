d571561c274e3c86605cec94cf66833cdef04517
zalkassem <z.arab.sy@gmail.com>
2024-01-06 17:37:13 +0100

    Update install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -63,7 +63,7 @@ sudo git clone https://github.com/zalkassem/wexDb.git
 cd wexDb/
 sudo cp -r products /var/www/cdn/"$3"
 sudo mkdir /var/www/cdn/"$3"/temp
-mongorestore --verbose --drop --gzip --host=127.0.0.1 --port=27017 --username=admin --password="$2" --authenticationDatabase=admin --nsInclude="wexcommerce.*" --archive=wexcommerce.gz
+#mongorestore --verbose --drop --gzip --host=127.0.0.1 --port=27017 --username=admin --password="$2" --authenticationDatabase=admin --nsInclude="wexcommerce.*" --archive=wexcommerce.gz
 sudo ufw enable
 sudo ufw allow ssh
 sudo ufw allow 22/tcp
