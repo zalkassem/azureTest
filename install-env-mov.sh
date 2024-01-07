eb0725693f29fb7594611809daf8e50049786915
zalkassem <z.arab.sy@gmail.com>
2024-01-07 12:11:07 +0100

    Update install-env-mov.sh

--- install-env-mov.sh
+++ install-env-mov.sh
@@ -38,9 +38,9 @@ sudo systemctl enable "$3"-frontend.service
 myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 sudo echo -e "NODE_ENV = production\nWC_PORT = 4004\nWC_HTTPS = false\nWC_PRIVATE_KEY = /etc/ssl/"$3".key\nWC_CERTIFICATE = /etc/ssl/"$3".crt\nWC_DB_HOST = 127.0.0.1\nWC_DB_PORT = 27017\nWC_DB_SSL = false\nWC_DB_SSL_KEY = /etc/ssl/"$3".key\nWC_DB_SSL_CERT = /etc/ssl/"$3".crt\nWC_DB_SSL_CA = /etc/ssl/"$3".ca.pem\nWC_DB_DEBUG = true\nWC_DB_APP_NAME = "$3"\nWC_DB_AUTH_SOURCE = admin\nWC_DB_USERNAME = admin\nWC_DB_PASSWORD = "$2"\nWC_DB_NAME = "$3"\nWC_JWT_SECRET = zeiad2023\nWC_JWT_EXPIRE_AT = 86400\nWC_TOKEN_EXPIRE_AT = 86400\nWC_SMTP_HOST = smtp.gmail.com\nWC_SMTP_PORT = 465\nWC_SMTP_USER = z.arab.sy@gmail.com\nWC_SMTP_PASS = "$1"\nWC_SMTP_FROM = z.arab.sy@gmail.com\nWC_ADMIN_EMAIL = z.arab.sy@gmail.com\nWC_CDN_PRODUCTS = /var/www/cdn/"$3"/products\nWC_CDN_TEMP_PRODUCTS = /var/www/cdn/"$3"/temp/products\nWC_BACKEND_HOST = http://"$myip":8002/\nWC_FRONTEND_HOST = http://"$myip":8001/\nWC_DEFAULT_LANGUAGE = en\nWC_DEFAULT_CURRENCY = $">> /opt/"$3"/api/.env
 sudo echo -e "NEXT_PUBLIC_WC_API_HOST = http://"$myip":4004\nNEXT_PUBLIC_WC_PAGE_SIZE = 30\nNEXT_PUBLIC_WC_CDN_PRODUCTS = http://"$myip"/cdn/"$3"/products\nNEXT_PUBLIC_WC_CDN_TEMP_PRODUCTS = http://"$myip"/cdn/"$3"/temp/products\nNEXT_PUBLIC_WC_APP_TYPE = backend">> /opt/"$3"/backend/.env
-sudo echo -e "NEXT_PUBLIC_WC_API_HOST = http://"$myip":4004\nNEXT_PUBLIC_WC_PAGE_SIZE = 30\nNEXT_PUBLIC_WC_CDN_PRODUCTS = http://"$myip"/cdn/"$3"/products\nNEXT_PUBLIC_WC_CDN_TEMP_PRODUCTS = http://"$myip"/cdn/"$3"/temp/products\nNEXT_PUBLIC_WC_APP_TYPE = frontend">> /opt/"$3"/frontend/.env
+sudo echo -e "REACT_APP_NODE_ENV=production\nREACT_APP_MI_API_HOST=http://"$myip":4004\nREACT_APP_MI_RECAPTCHA_ENABLED=false\nREACT_APP_MI_RECAPTCHA_SITE_KEY=GOOGLE_RECAPTCHA_SITE_KEY\nREACT_APP_MI_DEFAULT_LANGUAGE=en\nREACT_APP_MI_PAGE_SIZE=30\nREACT_APP_MI_PROPERTIES_PAGE_SIZE=15\nREACT_APP_MI_BOOKINGS_PAGE_SIZE=20\nREACT_APP_MI_BOOKINGS_MOBILE_PAGE_SIZE=10\nREACT_APP_MI_CDN_USERS=http://"$myip"/cdn/"$3"/users\nREACT_APP_MI_CDN_PROPERTIES=http://"$myip"/cdn/"$3"/properties\nREACT_APP_MI_AGENCY_IMAGE_WIDTH=60\nREACT_APP_MI_AGENCY_IMAGE_HEIGHT=30\nREACT_APP_MI_PROPERTY_IMAGE_WIDTH=300\nREACT_APP_MI_PROPERTY_IMAGE_HEIGHT=200\nREACT_APP_MI_MINIMUM_AGE=21\nREACT_APP_MI_PAGINATION_MODE=classic">> /opt/"$3"/frontend/.env
 sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-old
-sudo echo -e "server {\n\tlisten 80 default_server;\n\tserver_name _;\n\taccess_log /var/log/nginx/"$3".frontend.access.log;\n\terror_log /var/log/nginx/"$3".frontend.error.log;\n\n\tlocation / {\n\tproxy_pass http://localhost:8001;\n\tproxy_http_version 1.1;\n\tproxy_set_header Upgrade \$http_upgrade;\n\tproxy_set_header Connection 'upgrade';\n\tproxy_set_header Host \$host;\n\tproxy_cache_bypass \$http_upgrade;\n\t}\n\tlocation /cdn {\n\talias /var/www/cdn;\n\t}\n}\n\n\nserver {\n\tlisten 3000 default_server;\n\tserver_name _;\n\taccess_log /var/log/nginx/"$3".backend.access.log;\n\terror_log /var/log/nginx/"$3".backend.error.log;\n\n\tlocation / {\n\tproxy_pass http://localhost:8002;\n\tproxy_http_version 1.1;\n\tproxy_set_header Upgrade \$http_upgrade;\n\tproxy_set_header Connection 'upgrade';\n\tproxy_set_header Host \$host;\n\tproxy_cache_bypass \$http_upgrade;\n\t}\n}"> /etc/nginx/sites-available/default
+sudo echo -e "server {\n\troot /var/www/movinin/frontend;\n\tlisten 80 default_server;\n\tserver_name _;\n\taccess_log /var/log/nginx/"$3".frontend.access.log;\n\terror_log /var/log/nginx/"$3".frontend.error.log;\n\n\tlocation / {\n\t\ttry_files \$uri \$uri/ /index.html =404;\n\t}\n\tlocation /cdn {\n\t\talias /var/www/cdn;\n\t}\n}\n\n\nserver {\n\troot /var/www/movinin/backend;\n\tlisten 3003 default_server;\n\tserver_name _;\n\taccess_log /var/log/nginx/"$3".backend.access.log;\n\terror_log /var/log/nginx/"$3".backend.error.log;\n\n\tindex index.html;\n\n\tlocation / {\n\t\ttry_files \$uri \$uri/ /index.html =404;\n\t}\n}">> /etc/nginx/sites-available/default
 sudo nginx -t
 sudo systemctl restart nginx.service
 sudo systemctl status nginx.service
@@ -50,10 +50,13 @@ sudo cd /tmp
 sudo mkdir db
 cd db
 sudo git clone https://github.com/zalkassem/wexDb.git
-cd wexDb/
-sudo cp -r products /var/www/cdn/"$3"
+cd wexDb/mov
+sudo cp -r ./cdn/properties /var/www/cdn/"$3"
+sudo cp -r ./cdn/users /var/www/cdn/"$3"
 sudo mkdir /var/www/cdn/"$3"/temp
-mongorestore --verbose --drop --gzip --host=127.0.0.1 --port=27017 --username=admin --password="$2" --authenticationDatabase=admin --nsInclude="wexcommerce.*" --archive=wexcommerce.gz
+sudo mkdir /var/www/cdn/"$3"/temp/users
+sudo mkdir /var/www/cdn/"$3"/temp/properties
+mongorestore --verbose --drop --gzip --host=127.0.0.1 --port=27017 --username=admin --password="$2" --authenticationDatabase=admin --nsInclude="movinin.*" --archive=movinin.gz
 sudo ufw enable
 sudo ufw allow ssh
 sudo ufw allow 22/tcp
